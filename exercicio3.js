98b0c52b4e8c96cb764992bf4602819ce4815a55
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-22 08:30:39 -0300

    questoes-comentadas

---
+++ exercicio3.js
@@ -0,0 +1,4 @@
+//- Crie uma classe chamada `Livro` com as propriedades `titulo` (string), `autor` (string) e `anoPublicacao` (number).
+//- Implemente um método na classe chamado `obterInformacoes`, que retorna uma string formatada com as informações do livro.
+//- Crie três objetos da classe `Livro`, cada um representando um livro diferente.
+//- Exiba no console as informações de cada livro, utilizando o método `obterInformacoes`.
