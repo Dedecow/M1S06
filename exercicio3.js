545c8dbe5594245a82c92041229e7e8c3c8d4dce
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-23 11:22:46 -0300

    exercicio3-ideias-iniciais

--- exercicio3.js
+++ exercicio3.js
@@ -2,3 +2,19 @@
 //- Implemente um método na classe chamado `obterInformacoes`, que retorna uma string formatada com as informações do livro.
 //- Crie três objetos da classe `Livro`, cada um representando um livro diferente.
 //- Exiba no console as informações de cada livro, utilizando o método `obterInformacoes`.
+
+function criaLivro (titulo, autor, anoPublicacao){
+    let aluno = Object.create({});
+      livro.titulo = titulo;
+      livro.autor = autor;
+      livro.anoPublicacao = anoPublicacao;
+
+      return livro;
+  }
+  
+  // Criando livros para array
+let livro1 = criaLivro('O Guia do Mochileiro das Galáxias', 'Douglas Adams', 1979);
+let livro2 = criaLivro('O Senhor dos Anéis: A Sociedade do Anel', 'J.R.R. Tolkien', 1954);
+let livro3 = criaLivro('Harry Potter e a Pedra Filosofal', ' J.K. Rowling', 1997);
+
+
