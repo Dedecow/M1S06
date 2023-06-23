fde906db6f4457c65d751e7cbf9f539f13130ef0
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-06-23 10:41:24 -0300

    exercicio2Parte1

--- exercicio2.js
+++ exercicio2.js
@@ -2,3 +2,24 @@
 //- Utilize uma função de array, como `map`, `filter` ou `reduce`, para calcular a média das notas de cada aluno.
 //- Crie um novo array contendo objetos com as propriedades `nome` e `media`, onde `media` é a média das notas.
 //- Exiba o array resultante no console, ordenado em ordem decrescente de média.
+
+function criaAluno (nome, nota1, nota2){
+    let aluno = Object.create({});
+      aluno.nome = nome;
+      aluno.number1 = number1;
+      aluno.number2 = number2;
+
+      return aluno;
+  }
+  
+  // Criando alguns alunos
+let aluno1 = criaAluno('Nila', 9, 10);
+let aluno2 = criaAluno('Jaira', 7, 9);
+let aluno3 = criaAluno('André', 6, 5);
+
+// Exibindo os dados dos alunos
+console.log(aluno1);
+console.log(aluno2);
+console.log(aluno3);
+  
+let listaAlunos = [ aluno1, aluno2, aluno3];
